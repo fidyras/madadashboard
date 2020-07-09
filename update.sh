e2542e434655b7c562b75b8756af6ea303ce2046
fidyras <f.rasambainarivo@gmail.com>
2020-07-09 07:58:05 -0400

    Thu Jul  9 07:58:05 EDT 2020

--- update.sh
+++ update.sh
@@ -2,9 +2,9 @@
 
 
 # Whichever one you want to be the home page output as index.html
-Rscript -e 'rmarkdown::render("dashboard2.Rmd", output_file = "index.html", params = list(lang = "EN"))'
-Rscript -e 'rmarkdown::render("dashboard2.Rmd", output_file = "dashboard_FR.html", params = list(lang = "FR"))'
-Rscript -e 'rmarkdown::render("dashboard2.Rmd", output_file = "dashboard_MG.html", params = list(lang = "MG"))'
+Rscript -e 'rmarkdown::render("dashboard2.Rmd", output_file = "dashboard_EN.html", params = list(lang = "ENG"))'
+Rscript -e 'rmarkdown::render("dashboard2.Rmd", output_file = "dashboard_FR.html", params = list(lang = "FRA"))'
+Rscript -e 'rmarkdown::render("dashboard2.Rmd", output_file = "index.html", params = list(lang = "MDG"))'
 
 # Stage files to be committed
 git add \*.md \*.Rmd \*.html
